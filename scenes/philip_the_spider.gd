fadf7b66c2fcde5db4e003ffbb544f9114e885db
crystal <cheesedrone@gmail.com>
2025-10-14 18:49:39 -0700

    string physics
    bouncing is wonky

--- scenes/philip_the_spider.gd
+++ scenes/philip_the_spider.gd
@@ -6,14 +6,37 @@ const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	print(position.y)
-	if (not is_on_floor()): # --> is y > ###
-		velocity += get_gravity() * delta * 0.6
+	print(velocity.y)
+	
+	# add the gravity
+	velocity += get_gravity()* delta*2
+	
+	# bounce back
+	if position.y > 300:
+		velocity.y -= position.y/10	
+	if position.y > 370:
+		velocity.y -= position.y/5
+	velocity.y += 1
+	
+	if position.y > 320:
+		if velocity.y > 0:
+			velocity.y -= (position.y - 320)*4
+		if velocity.y > 0:
+			velocity.y += (position.y - 320)*4
+	if position.y < 320:
+		if velocity.y < 0:
+			velocity.y -= (320 - position.y)*4
+		if velocity.y < 0:
+			velocity.y += (320 - position.y)*4
 
 	# Handle jump.
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("up"):
 		velocity.y = JUMP_VELOCITY
+		velocity += get_gravity()* delta * 0.5
+		
+#	if Input.is_action_pressed("down"):
+#		velocity.y = -JUMP_VELOCITY
+#		velocity += get_gravity()* delta * 0.8
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
